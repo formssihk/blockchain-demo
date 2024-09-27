c029c2f30d37f7cac25e2d76f979374bd3ad10cf
alice <aliceng223@gmail.com>
2024-09-27 15:30:53 +0800

    FE formatting

--- frontend/src/components/Blockchain.jsx
+++ frontend/src/components/Blockchain.jsx
@@ -4,13 +4,12 @@ import Block from './Block';
 function Blockchain({ clientId, blocks, updateBlock, rehashBlock, nodeIndex, showTick, confirmBlock }) {
   const storedClientId = localStorage.getItem('clientId');
   return (
-    <>
+    <div>
       <div>
         <h2 className="text-lg font-bold mt-2">{storedClientId === clientId ? "My Node": `Node ${clientId}`}</h2>
       </div>
       <div 
         className="w-full p-4 border rounded flex overflow-x-scroll space-x-4"
-        style={{ maxWidth: '100%' }} // Ensure max-width for scrolling
       >
         {blocks && blocks.map((block, index) => (
           <Block
@@ -26,7 +25,7 @@ function Blockchain({ clientId, blocks, updateBlock, rehashBlock, nodeIndex, sho
           />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
